a013bd7f137254ded1b1222412376fde51102c0a
Thomas Cohn <cohnt@umich.edu>
2021-04-19 16:51:07 -0400

    Simplify the code a bit.

--- fake_features.py
+++ fake_features.py
@@ -9,14 +9,10 @@ from odometry_classes import MapPoint, Frame, Map
 def create_random_fake_features(camera_pose, count, random_range=(1,1,1), z_min=0.1):
 	# Camera poses are given as transformations from local to global frame
 	# See: https://www.eth3d.net/slam_documentation
-	rot_mat = homogenize_matrix(quat_to_mat(camera_pose.quat))
-	trans_mat = make_translation_matrix(camera_pose.pos)
-	full_mat = np.matmul(trans_mat, rot_mat) # Rotate, then translate
+	# Returns random 3d points in the world frame, located in front of the given camera pose
+	# The output, points_global, will be a 4xn set of homogeneous vectors
 	lows = (-random_range[0], -random_range[1], z_min) # Can't have negative z
 	highs = (random_range[0], random_range[1], random_range[2])
 	points_local = homogenize_vectors(np.random.uniform(low=lows, high=highs, size=(count,3)).T)
-	points_global = np.matmul(full_mat, points_local)
+	points_global = local_xyz_to_global_xyz(camera_pose, points_local)
 	return points_global
-
-def project_fake_feature_to_image(feature):
-	pass
