b3d0355af0891d3e05fa29f2cf17dc239be0dc91
Thomas Cohn <cohnt@umich.edu>
2021-04-20 12:04:41 -0400

    Create the fake points, project the into the camera frame, and display.

--- fake_features_odometry_test.py
+++ fake_features_odometry_test.py
@@ -11,3 +11,12 @@ dataset_name = "plant_1"
 data = Dataloader(dataset_name)
 
 slam = SLAM()
+
+fake_points = fake_features.create_random_fake_map_points(data.image_groundtruths[0], 25)
+map_points_vec = np.array([fake_point.pos[:,0] for fake_point in fake_points]).T
+descriptors_vec = np.array([fake_point.descriptor for fake_point in fake_points])
+img_points = local_xyz_to_uv(data.intrinsic_mat, global_xyz_to_local_xyz(data.image_groundtruths[0], map_points_vec))
+
+plt.imshow(data.images[0])
+plt.scatter(img_points[0], img_points[1])
+plt.show()
