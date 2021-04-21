42c6ae37bcc817d7581c9d24c5af4d34c3b2dda4
Thomas Cohn <cohnt@umich.edu>
2021-04-21 16:13:34 -0400

    Make frames for every image.

--- fake_features_odometry_test.py
+++ fake_features_odometry_test.py
@@ -17,10 +17,7 @@ def fake_match(desc1, desc2):
 
 slam = SLAM(fake_match)
 
-fake_points = fake_features.create_random_fake_map_points(data.image_groundtruths[0], 5)
-# map_points_vec = np.array([fake_point.pos[:,0] for fake_point in fake_points]).T
-# map_points_vec = np.concatenate([fake_point.pos for fake_point in fake_points], axis=1)
-# descriptors_vec = np.array([fake_point.descriptor for fake_point in fake_points])
+fake_points = fake_features.create_random_fake_map_points(data.image_groundtruths[0], 15)
 
 def fake_create_input_frame(index, fake_points):
 	img = data.images[index]
@@ -35,19 +32,14 @@ def fake_create_input_frame(index, fake_points):
 	final_descriptors = (fake_points_descriptors[idx1])[idx2]
 	return Frame(img, final_points, final_descriptors)
 
-# img0_points = only_within_image(data.images[0].shape, local_xyz_to_uv(data.intrinsic_mat, only_positive_z(global_xyz_to_local_xyz(data.image_groundtruths[0], map_points_vec))))
-# img20_points = only_within_image(data.images[20].shape, local_xyz_to_uv(data.intrinsic_mat, only_positive_z(global_xyz_to_local_xyz(data.image_groundtruths[20], map_points_vec))))
-
-f0 = fake_create_input_frame(0, fake_points)
-f20 = fake_create_input_frame(20, fake_points)
+frames = []
+n_images = len(data.images)
+for i in range(n_images):
+	frames.append(fake_create_input_frame(i, fake_points))
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
-# ax1.imshow(data.images[0])
-# ax1.scatter(img0_points[0], img0_points[1])
-ax1.imshow(f0.img)
-ax1.scatter(f0.keypoints[0], f0.keypoints[1], c=f0.descriptors, cmap=plt.get_cmap("Set1"))
-# ax2.imshow(data.images[20])
-# ax2.scatter(img20_points[0], img20_points[1])
-ax2.imshow(f20.img)
-ax2.scatter(f20.keypoints[0], f20.keypoints[1], c=f20.descriptors, cmap=plt.get_cmap("Set1"))
+ax1.imshow(frames[0].img)
+ax1.scatter(frames[0].keypoints[0], frames[0].keypoints[1], c=frames[0].descriptors, cmap=plt.get_cmap("Set1"))
+ax2.imshow(frames[25].img)
+ax2.scatter(frames[25].keypoints[0], frames[25].keypoints[1], c=frames[25].descriptors, cmap=plt.get_cmap("Set1"))
 plt.show()
