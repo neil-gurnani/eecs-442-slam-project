Scaling was being done after the translation was used for some stuff. Fixing that has fixed the map points error in the initialization.

geometry.py
@@ -148,7 +148,7 @@ def local_only_good_image_idx(intrinsic_mat, image_shape, xyz):
 	return uv[:,mask], np.where(mask)[0]
 	# NOTE: This could probably be optimized, so invalid points aren't projeced and such
 
-def triangulate(old_points, new_points, camera_mat):
+def triangulate(old_points, new_points, camera_mat, scale):
 	# old_points and new_points should be 2xn sets of vectors
 	# camera_mat should be the intrinsic matrix, either 3x3 or 3x4
 	# Returns the 3d points as a 4xn set of homogeneous vectors in the coordinate frame of the new image
@@ -173,8 +173,9 @@ def triangulate(old_points, new_points, camera_mat):
 
 	# Recover camera pose
 	points, R, t, mask = cv2.recoverPose(mat, good_new_points_norm, good_old_points_norm)
-	M_new = np.hstack((R, t))
-	M_old = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
+	t = (t / np.linalg.norm(t)) * scale # Scale for the first displacement only
+	M_new = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
+	M_old = np.hstack((R, t))
 	P_new = np.dot(camera_mat_3x3,  M_new)
 	P_old = np.dot(camera_mat_3x3,  M_old)
 

odometry_classes.py
@@ -63,8 +63,7 @@ class SLAM():
 		descriptors = self.init_frame.descriptors[pairs[:,0]]
 
 		# Normalize
-		point_4d, R, t, mask = triangulate(start_points, next_points, frame.intrinsic_mat)
-		t = (t / np.linalg.norm(t)) * scale # Scale for the first displacement only
+		point_4d, R, t, mask = triangulate(start_points, next_points, frame.intrinsic_mat, scale)
 		descriptors = descriptors[mask]
 
 		# Compue the camera and point positions in the global frame
@@ -84,9 +83,13 @@ class SLAM():
 		next_vecs = next_vecs / np.linalg.norm(next_vecs, axis=0)
 		dprods = np.sum(np.multiply(start_vecs, next_vecs), axis=0)
 
-		# import matplotlib.pyplot as plt
+		import matplotlib.pyplot as plt
+		# plt.scatter(next_points[0], next_points[1], color="blue", s=20**2)
+		# plt.scatter(local_xyz_to_uv(frame.intrinsic_mat, point_4d)[0], local_xyz_to_uv(frame.intrinsic_mat, point_4d)[1], color="red", s=8**2)
+		# plt.scatter(global_xyz_to_uv(new_pose, frame.intrinsic_mat, points_global)[0], global_xyz_to_uv(new_pose, frame.intrinsic_mat, points_global)[1], color="green", s=2**2)
+		# plt.show()
 		# plt.scatter(start_points[0], start_points[1], color="blue", s=20**2)
-		# plt.scatter(local_xyz_to_uv(frame.intrinsic_mat, point_4d)[0], local_xyz_to_uv(frame.intrinsic_mat, point_4d)[1], color="red")
+		# plt.scatter(global_xyz_to_uv(self.init_pose, self.init_frame.intrinsic_mat, points_global)[0], global_xyz_to_uv(self.init_pose, self.init_frame.intrinsic_mat, points_global)[1], color="green", s=2**2)
 		# plt.show()
 
 		# Remove points with insufficient parallax